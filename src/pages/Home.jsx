0e0a8653e8e9e28fc4c4b11fbd37477db9e70f05
ZAYAN SHOPPING INDIA <zayanshoppingindia@gmail.com>
2026-01-04 02:05:47 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { base44 } from '@/api/base44Client';
 import HeroSection from '@/components/home/HeroSection';
 import ServicesSection from '@/components/home/ServicesSection';
 import PricingSection from '@/components/home/PricingSection';
